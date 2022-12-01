e40ff354ddcd7e1d0f6871a21b9bddeb3b74edec
youssefchlendi <youssef.chlendi@gmail.com>
2022-12-01 20:37:25 +0100

    last state

---
+++ microservice-application/demo/src/main/java/com/example/demo/Cont.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Cont {
+
+	@GetMapping("/hello")
+	public void hypotheticalEndpoint(@RequestHeader("Authorization") String authorization) {
+		System.out.println(authorization);
+	}
+
+}
